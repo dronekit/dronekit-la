17f94ba7d7bf16ba77cae4ce50baab734fbb5dfb
Peter Barker <pbarker@barker.dropbear.id.au>
2015-08-25 09:31:16 +1000

    la: add more tests

--- test/test.sh
+++ test/test.sh
@@ -5,9 +5,12 @@ ANALYZER=../dataflash_logger
 set -e
 set -x
 
+$ANALYZER logs/1.solo.tlog >/dev/null
 $ANALYZER -c /dev/null logs/1.solo.tlog >/dev/null
 $ANALYZER -c /dev/null -s json logs/1.solo.tlog >/dev/null
 $ANALYZER -h
+$ANALYZER -s bad_output_format logs/1.solo.tlog
+$ANALYZER logs/this-log-does-not-exist
 $ANALYZER -s json logs/1.solo.tlog >/dev/null
 $ANALYZER -c very_unlikely_to_exist json logs/1.solo.tlog >/dev/null
 
