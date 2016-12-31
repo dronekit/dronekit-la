494545383a96a1505d63b5e3fe611cd62c3f0c66
Peter Barker <pbarker@barker.dropbear.id.au>
2016-12-31 15:09:52 +1100

    mavlink_message_handler: remove bogus comment

--- mavlink_message_handler.cpp
+++ mavlink_message_handler.cpp
@@ -3,17 +3,6 @@
 #include "la-log.h"
 #include <errno.h>
 
-// void MAVLink_Message_Handler::send_message_to_telem_forwarder(mavlink_message_t &msg)
-// {
-//   uint8_t sendbuf[512];
-
-//   uint16_t messageLen = mavlink_msg_to_send_buffer((uint8_t*)sendbuf,&msg);
-
-//   if (sendto(_fd_telem_forwarder, sendbuf, messageLen, 0, (struct sockaddr *)_sa_telemetry_forwarder, sizeof(struct sockaddr)) == -1) {
-//     la_log(LOG_INFO, "Failed sendto: %s", strerror(errno));
-//   }
-// }
-
 /// @brief Decode and handle a mavlink message.
 /// @param timestamp The time a mavlink packet arrived.
 /// @param msg The mavlink message.
