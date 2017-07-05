761dd3391fa35d8dabff4393c36d46500bf8d00d
Lucas De Marchi <lucas.demarchi@intel.com>
2017-07-05 10:43:35 -0700

    build: fix cluttering submodule
    Output should not being generated on the submodule since it upsets git
    about untracked content.

--- mavlink_writer.h
+++ mavlink_writer.h
@@ -5,7 +5,7 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpedantic"
-#include "mavlink/c_library/ardupilotmega/mavlink.h"
+#include "mavlink_c_library/ardupilotmega/mavlink.h"
 #pragma GCC diagnostic pop
 
 #include "INIReader.h"
