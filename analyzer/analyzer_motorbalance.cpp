08a816568e17d71677a24cf8dd7237dd894ba976
Peter Barker <pbarker@barker.dropbear.id.au>
2019-09-30 23:48:19 +1000

    analyzer_motorbalance: add tailsitter frame type

--- analyzer/analyzer_motorbalance.cpp
+++ analyzer/analyzer_motorbalance.cpp
@@ -128,8 +128,12 @@ uint8_t Analyzer_MotorBalance::motorcount_for_frame_class(uint8_t value)
     case 7:
         ret = 3;
         break;
+    case 10:
+        // tailsitter
+        ret = 1; // note that this could be incorrect!
+        break;
     default:
-        ::fprintf(stderr, "Umknown frame value (%u)\n", value);
+        ::fprintf(stderr, "Umknown FRAME_CLASS value (%u)\n", value);
         abort();
     }
     return ret;
